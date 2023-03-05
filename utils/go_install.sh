e5257238e05c6b78170cad0aed5ca4fb0b61c399
marutyan <fastrpl@gmail.com>
2023-03-05 05:13:32 +0100

    add utils

---
+++ utils/go_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd $HOME
+if ! [ -x "$(command -v go)" ]; then
+VER="1.20"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm -rf  "go$VER.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+fi
