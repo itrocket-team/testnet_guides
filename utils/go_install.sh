d0272441b265287f9a38581e2ce7e80d5bf5469e
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-03-05 09:41:05 +0400

    Add .bash_profile

--- utils/go_install.sh
+++ utils/go_install.sh
@@ -7,6 +7,10 @@ wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
 rm -rf  "go$VER.linux-amd64.tar.gz"
+if [ ! -d "$HOME/.bash_profile" ]; then
+touch $HOME/.bash_profile
+source $HOME/.bash_profile
+fi
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 fi
