cb1322fdb451a6f8dd49a20b7226ad64f0c2d57b
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-05-12 18:30:54 +0400

    Create common.sh

---
+++ utils/common.sh
@@ -0,0 +1,19 @@
+GREEN="\e[1m\e[1;32m" # green color
+RED="\e[1m\e[1;31m" # red color
+NC="\e[0m"           # no color
+
+function printLogo {
+  bash <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/itrocket_logo.sh)
+}
+
+function printLine {
+  echo "------------------------------------------------------------------------------------"
+}
+
+function printGreen {
+  echo -e "${CYAN}${1}${NC}"
+}
+
+function printRed {
+  echo -e "${RED}${1}${NC}"
+}
