994d0bc5788159c700782620487bb32f830e2cd6
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-05-12 22:42:59 +0400

    Update upgrade.sh

--- utils/upgrade.sh
+++ utils/upgrade.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
+sudo -v
 source <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/common.sh)
 
-while getopts u:v:n:i:b:h:p: flag; do
+while getopts u:b:v:o:n:p:h:i:r: flag; do
   case "${flag}" in
   u) UPD_HEIGHT=$OPTARG ;;
-  v) VERSION=$OPTARG ;;
-  n) CHAIN_NAME=$OPTARG ;;
-  i) CHAIN_ID=$OPTARG ;;
   b) BINARY=$OPTARG ;;
+  v) VERSION=$OPTARG ;;
+  o) OLD_BIN=$OPTARG ;;
+  n) NEW_BIN=$OPTARG ;;
+  p) PROJECT_NAME=$OPTARG ;;
   h) PROJECT_HOME=$OPTARG ;;
-  p) PORT_RPC=$OPTARG ;;
+  i) CHAIN_ID=$OPTARG ;;
+  r) PORT_RPC=$OPTARG ;;
   *) echo "WARN: unknown parameter: ${OPTARG}"
   esac
 done
 
 printLogo
-sleep 3
-
-echo -e "$GREEN NODE WILL BE UPDATED AT BLOCK: $HEIGHT TO $VERSION{NC}"
+echo -e "$GREEN $PROJECT_NAME YOUR NODE WILL BE UPDATED AT BLOCK $UPD_HEIGHT${NC}"
+sleep 2
 for((;;)); do
-  elysd status
   height=$(${BINARY} status |& jq -r .SyncInfo.latest_block_height)
     if ((height==$UPD_HEIGHT)); then
-      mv /home/elys/elys/build/elysd /home/elys/go/bin/elysd
-    systemctl restart $BINARY
-      echo restart
+      sudo mv $NEW_BIN $OLD_BIN
+      sudo systemctl restart $BINARY
+      echo -e "$GREEN restarting...${NC}"
+      
     break
   else
       echo $height
   fi
-  sleep 5
+  sleep 4
 done
-sleep 600
-#tmux kill-session
+journalctl -u $BINARY -f
