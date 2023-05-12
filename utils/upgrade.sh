600bf21fb337ca01b9d946582ed5f98714472826
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-05-12 21:57:26 +0400

    Update upgrade.sh

--- utils/upgrade.sh
+++ utils/upgrade.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 source <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/common.sh)
 
-#while getopts u:v:n:i:b:h:p: flag; do
-  #case "${flag}" in
-  #u) UPD_HEIGHT=$OPTARG ;;
-  #v) VERSION=$OPTARG ;;
-  #n) CHAIN_NAME=$OPTARG ;;
-  #i) CHAIN_ID=$OPTARG ;;
-  #b) BINARY=$OPTARG ;;
-  #h) PROJECT_HOME=$OPTARG ;;
-  #p) PORT_RPC=$OPTARG ;;
-  #*) echo "WARN: unknown parameter: ${OPTARG}"
-  #esac
-#done
+while getopts u:v:n:i:b:h:p: flag; do
+  case "${flag}" in
+  u) UPD_HEIGHT=$OPTARG ;;
+  v) VERSION=$OPTARG ;;
+  n) CHAIN_NAME=$OPTARG ;;
+  i) CHAIN_ID=$OPTARG ;;
+  b) BINARY=$OPTARG ;;
+  h) PROJECT_HOME=$OPTARG ;;
+  p) PORT_RPC=$OPTARG ;;
+  *) echo "WARN: unknown parameter: ${OPTARG}"
+  esac
+ done
 
 printLogo
 
