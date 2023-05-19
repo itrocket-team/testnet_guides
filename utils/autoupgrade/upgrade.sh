7de94a5af30e077baeec5c8c2aa82520420d849f
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-05-19 15:37:28 +0400

    Update upgrade.sh

--- utils/autoupgrade/upgrade.sh
+++ utils/autoupgrade/upgrade.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo -v
 source <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/common.sh)
 
 while getopts u:b:v:n:p:h:i:r: flag; do
@@ -41,8 +40,7 @@ for((;;)); do
     if ((height==$UPD_HEIGHT)); then
       sudo mv $NEW_BIN_PATH $(which $BINARY)
       sudo systemctl restart $BINARY
-      echo -e "$GREEN restarting...${NC}"
-      
+      echo -e "$GREEN restarting...${NC}"      
     break
   else
       echo $height
