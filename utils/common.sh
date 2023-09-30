2e0186cce54241118c1d2a65b190248eb44fec74
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-09-30 14:04:05 +0400

    Add blue color

--- utils/common.sh
+++ utils/common.sh
@@ -1,6 +1,7 @@
 GREEN="\e[1m\e[1;32m" # green color
 RED="\e[1m\e[1;31m" # red color
-NC="\e[0m"           # no color
+BLUE='\033[0;34m'   # blue color
+NC="\e[0m"          # no color
 
 function printLogo {
   bash <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/logo.sh)
@@ -16,6 +17,10 @@ function printGreen {
 
 function printRed {
   echo -e "${RED}${1}${NC}"
+}
+
+function printBlue {
+  echo -e "${BLUE}${1}${NC}"
 }
 
 function addToPath {
