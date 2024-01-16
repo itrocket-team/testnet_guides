414de7adc30f5bc2ec3a878172bd38075e97937e
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2024-01-16 18:42:58 +0400

    Enable buy rolls function

--- massa/massa-monitoring.sh
+++ massa/massa-monitoring.sh
@@ -4,11 +4,13 @@
 # Configure telegram bot and chat id token
 BOT_TOKEN="<YOUR_BOT_TOKEN>"
 CHAT_ID_ALARM="<YOUR_CHAT_ID>"
+# You can add NODE_NAME to customize Telegram messages
+NODE_NAME=""
 
 # set vars
-read -s -p "Enter massa-client password: " PASSWORD
+read -s -p "Enter ${NODE_NAME} massa-client password: " PASSWORD
 PASSWORD=$PASSWORD
-SLEEP=900
+SLEEP=15m
 DIR=$(pwd)
 HOME_DIR=${DIR}/massa/massa-client
 cd $HOME_DIR
@@ -29,35 +31,46 @@ echo '----------------------'
 STATUS=$(./massa-client -p "$PASSWORD" get_status | grep 'Error'| awk '{print $1}')
 while [ "$STATUS" == "Error:" ]
 do
-   MESSAGE="Massa node status is: Error, please check it"
+   MESSAGE="${NODE_NAME} Massa node status is: Error, please check it"
    curl --header 'Content-Type: application/json' --request 'POST' --data '{"chat_id":"'"${CHAT_ID_ALARM}"'", "text":"'"$(echo -e "${MESSAGE}")"'", "parse_mode": "html"}' "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" /dev/null 2>&1
-  echo -e "\033[0;31m"$MESSAGE", waiting 60 min...\033[0m"
- sleep 3600
+  echo -e "\033[0;31m"$MESSAGE", waiting ${SLEEP}...\033[0m"
+ sleep $SLEEP
 STATUS=$(./massa-client -p "$PASSWORD" get_status | grep 'Error'| awk '{print $1}')
 WALLET_ADDRESS=$(./massa-client wallet_info -p "$PASSWORD" | grep 'Address' | awk '{print $2}')
 done
  
 # Checking candidate rolls
 CANDIDATE=$(./massa-client wallet_info -p "$PASSWORD" | grep Rolls | awk '{print $4}' | sed 's/=/ /'|awk '{print $2}')
-echo There is a $CANDIDATE candidate rolls
 
 if [ "$CANDIDATE" != "0" ]; then
- echo -e "\033[0;32m"Candidate = $CANDIDATE rolls, waiting $SLEEP sec..."\033[0m"
+BALANCE=$(./massa-client wallet_info -p "$PASSWORD" | grep "Balance:" |  awk '{print $2}' | sed 's/=/ /' |  awk '{print $2}' | awk '{print int($1)}')
+
+if [ "$BALANCE" -gt "101" ]; then
+    ROLLS_TO_BUY=$((BALANCE / 100))
+    echo -e "\033[0;32m"Candidate=${CANDIDATE} Rolls, Balance=${BALANCE} MAS. Buing $ROLLS_TO_BUY rolls..."\033[0m"
+    TXH=$(./massa-client -p "$PASSWORD" buy_rolls ${WALLET_ADDRESS} ${ROLLS_TO_BUY} 0)
+    echo "We have bought ${ROLLS_TO_BUY} rolls TXH:$TXH"
+else
+    echo "Balance=${BALANCE} MAS, insufficient to buy additional Rolls"
+fi
+ echo -e "\033[0;32m"Candidate=${CANDIDATE} Rolls, waiting ${SLEEP}..."\033[0m"
  echo '-------------------------------------------------'
-sleep $SLEEP
+ sleep $SLEEP
 else
 echo -e "\033[0;32m"Checking ballance..."\033[0m"
 BALANCE=$(./massa-client wallet_info -p "$PASSWORD" | grep "Balance:" |  awk '{print $2}' | sed 's/=/ /' |  awk '{print $2}' | awk '{print int($1)}')
-echo -e "\033[0;32m"Ballance: $BALANCE rolls"\033[0m"
-    MESSAGE="Massa node have $CANDIDATE rolls, please check it"
+echo -e "\033[0;32m"Ballance: ${BALANCE} MAS"\033[0m"
+    MESSAGE="${NODE_NAME} Massa node have $CANDIDATE Rolls, please check it"
    curl --header 'Content-Type: application/json' --request 'POST' --data '{"chat_id":"'"${CHAT_ID_ALARM}"'", "text":"'"$(echo -e "${MESSAGE}")"'", "parse_mode": "html"}' "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" /dev/null 2>&1
-if [ "$BALANCE" -gt "99" ]
+if [ "$BALANCE" -gt "101" ]
 then
-echo -e "\033[0;32m"Buing rolls to $WALLET_ADDRESS, function disabled..."\033[0m"
-#TXH=$(./massa-client -p "$PASSWORD" buy_rolls ${WALLET_ADDRESS} 1 0)
-#echo We have buy 1 rools TXH:$TXH
+ROLLS_TO_BUY=$((BALANCE / 100))
+echo -e "\033[0;32m"Buing rolls to ${WALLET_ADDRESS}..."\033[0m"
+TXH=$(./massa-client -p "$PASSWORD" buy_rolls ${WALLET_ADDRESS} ${ROLLS_TO_BUY} 0)
+echo We have buy ${ROLLS_TO_BUY} Rolls TXH:$TXH
 fi
-echo waiting $SLEEP sec
+
+echo waiting $SLEEP
 sleep $SLEEP
 fi
 done
